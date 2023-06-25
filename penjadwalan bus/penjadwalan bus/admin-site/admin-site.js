65439d311756845aba4789a800c2768c1c5a0eba
RakaSatya <129923734+RakaSatya@users.noreply.github.com>
2023-06-25 17:46:27 +0700

    Add files via upload

---
+++ penjadwalan bus/penjadwalan bus/admin-site/admin-site.js
@@ -0,0 +1,119 @@
+// Dark Mode and Close Nav
+
+// Check if 'darkMode' exists in localStorage, if not, set it to 'false'
+if (localStorage.getItem('darkMode') === null) {
+    localStorage.setItem('darkMode', 'false');
+}
+  
+// Get the current dark mode value from localStorage and convert it to a boolean
+let darkMode = localStorage.getItem('darkMode') === 'true';
+  
+// Apply the dark mode class to the body on page load
+document.querySelector("body").classList.toggle("dark", darkMode);
+
+document.querySelector(".mode").addEventListener("click", () => {
+    // Toggle the dark mode value
+    darkMode = !darkMode;
+
+    // Apply the dark mode class to the body
+    document.querySelector("body").classList.toggle("dark", darkMode);
+
+    // Update the dark mode value in localStorage as a string
+    localStorage.setItem('darkMode', darkMode.toString());
+});
+
+document.querySelector(".close-btn").
+addEventListener("click", function(){
+    document.querySelector(".nav").classList.toggle("close");
+    document.querySelector(".close-btn i").classList.toggle("closed");
+})
+
+// Create Calendar
+
+const daysTag = document.querySelector(".days"),
+currentDate = document.querySelector(".date"),
+prevNextIcon = document.querySelectorAll(".month-year i");
+
+
+// CurrYear -> STRING
+// CurrMonth -> STRING
+
+let currYear = localStorage.getItem('currYear') || new Date().getFullYear();
+let currMonth = localStorage.getItem('currMonth') || new Date().getMonth();
+
+
+// storing full name of all months in array
+const months = ["January", "February", "March", "April", "May", "June", "July",
+              "August", "September", "October", "November", "December"];
+
+
+
+const renderCalendar = () => {
+    let monthplusone = parseInt(currMonth) + 1;// Convert STRING to INT and then + 1
+
+    let firstDayofMonth = new Date(currYear, currMonth, 1).getDay(), // getting first day of month
+    lastDateofMonth = new Date(currYear, monthplusone, 0).getDate(), // getting last date of month
+    lastDayofMonth = new Date(currYear, currMonth, lastDateofMonth).getDay(), // getting last day of month
+    lastDateofLastMonth = new Date(currYear, currMonth, 0).getDate(); // getting last date of previous month
+    let liTag = "";
+
+    for (let i = firstDayofMonth; i > 0; i--) { 
+        // creating li of previous month last days
+        liTag += `<div class="day prev-day">${lastDateofLastMonth - i + 1}</div>`;
+    }
+    
+    for (let i = 1; i <= lastDateofMonth; i++) { 
+        // creating li of all days of current month
+        // adding active class to li if the current day, month, and year matched
+        liTag += `<div class="day"><button type = "submit" name = "select_date"
+        value = "${currYear}-${monthplusone}-${i}">${i}</button></div>`;
+    }
+    let n = 6;
+    if((firstDayofMonth + lastDateofMonth) <= 35){
+        n = 13;
+    }
+    for (let i = lastDayofMonth; i < n; i++) { 
+        // creating li of next month first days
+        liTag += `<div class="day next-day">${i - lastDayofMonth + 1}</div>`
+    }
+    currentDate.innerText = `${months[currMonth]} ${currYear}`; // passing current mon and yr as currentDate text
+    daysTag.innerHTML = liTag;
+}   
+
+renderCalendar();
+
+prevNextIcon.forEach(icon => {
+     // getting prev and next icons
+    icon.addEventListener("click", () => { // adding click event on both icons
+        // if clicked icon is previous icon then decrement current month by 1 else increment it by 1
+        if (icon.id === "prev") {
+            currMonth--;
+            if (currMonth < 0) {
+                currMonth = 11;
+                currYear--;
+        }
+        } else {
+            currMonth++;
+            if (currMonth > 11) {
+                currMonth = 0;
+                currYear++;
+            }
+        }
+        localStorage.setItem('currYear', currYear);
+        localStorage.setItem('currMonth', currMonth);
+        //alert(currYear + "-" + currMonth + "-" + "9");
+        renderCalendar(); // calling renderC    alendar function
+    });
+});
+
+// prevent the currYear and currMonth from getting re-initiate to current realtime date on refresh
+window.addEventListener("beforeunload", () => {
+    let session = sessionStorage.getItem('register');
+
+    if (session == null) {
+        localStorage.removeItem('currYear');
+        localStorage.removeItem('currMonth');
+
+    }
+    sessionStorage.setItem('register', 1);
+});
---
+++ penjadwalan bus/penjadwalan bus/index/index.php
@@ -0,0 +1,97 @@
+<?php include('../php/server.php')
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Index</title>
+
+    <!--- === STYLES === -->
+    <link rel="stylesheet" href="../styles/global.css">
+    <link rel="stylesheet" href="index.css">
+    <!--- === ICON === -->
+    <link rel="stylesheet" href="https://unicons.iconscout.com/release/v4.0.8/css/line.css">
+</head>
+<body class="index">
+    <div class="nav">
+        <div class="logo-img">
+            <img src="../images/logo.png">
+        </div>
+        <ul class="menu-link">
+            <li class="left"><a href="#" class="nav-item gradient-text" >Home</a></li>
+            <li class="left"><a href="#" class="nav-item gradient-text" >About Us</a></li>
+            <li class="left"><a href="#" class="nav-item gradient-text" >Order Ticket</a></li>
+            <li class="left"><a href="#" class="nav-item gradient-text" >Contact</a></li>
+            <li class="right login-link"><a href="
+                <?php 
+                if(isset($_SESSION['loginStatus'])) echo "../users/profile.php";
+                else echo "#";
+                ?>" class="nav-item gradient-text">
+                <?Php 
+                if(isset($_SESSION['loginStatus'])) echo $_SESSION['username'];
+                else echo "Log In";
+                ?>
+            </a></li>
+            <li class="right"><a href="../admin-site/admin-site.php" class="nav-item gradient-text">
+                <?php if(isset($_SESSION['admin'])) echo "Admin-Site";
+                ?>
+            </a></li>
+        </ul>
+    </div>
+    <div class="overlay">
+        <div class="form-wrap">
+            <div class="close-btn">
+                <i class="uil uil-times-circle"></i>
+            </div>
+            <div class="login">
+                <h2 class="form-title">Log In</h2>
+                <form action = "index.php" method = "POST">
+                    <input type="text" name = "username" placeholder="Username/Email" required>
+                    <input type="password" name = "password" placeholder="Password" required>
+                    <input type="submit" name = "loginbtn" value="Log In">
+                </form>
+                <div class="pass-reg-link">
+                    <div class="forgot-pass">
+                        <a href="#">Forgot Pass?</a>
+                    </div>
+                    <div class="register-link" id="registerBtn">
+                        <a href="#">Register Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="register inactive">
+                <h2 class="form-title">Registration</h2>
+                <div class="errorMsg">
+                    <?php
+                    if(isset($_SESSION['errormsg'])){
+                        echo $_SESSION['errormsg'];
+                    }
+                    ?>
+                </div>
+                <form action="index.php" method = "POST" >
+                    <input type="text" name = "email"placeholder="Email">
+                    <input type="text" name = "username" placeholder="Username">
+                    <input type="password" name = "password_1" placeholder="Password">
+                    <input type="password2" name = "password_2" placeholder="Confirm Password">
+                    <input type="submit" name = "regBtn" value="Register">
+                </form>
+                <div class="alr-have-acc">
+                    <p>Already have an account?</p>
+                    <a href="#" id="loginBtn">Log In</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="index.js"></script>
+    
+    <?php // If there's an error
+    if (isset($_SESSION['error'])) { ?>
+        <script>
+            alert("<?php echo $_SESSION['error']; ?>");
+            <?php unset($_SESSION['error']); ?>
+        </script>
+    <?php } ?>
+</body>
+</html>
